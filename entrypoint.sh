9fe3f30cd33be842993f6aeefc5595386ad8ba76
AlessandroMinoccheri <alessandro.minoccheri@ideato.info>
2022-01-09 23:29:05 +0100

    added argument to use target php version

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,8 +12,13 @@ if [ "$REQUIRE_DEV" = "true" ]; then
     NO_DEV=""
 fi
 
+TARGET_PHP_VERSION=""
+if [ "TARGET_PHP_VERSION" != "" ]; then
+    TARGET_PHP_VERSION=" --target-php-version=$TARGET_PHP_VERSION"
+fi
+
 COMPOSER_COMMAND="composer install --no-progress $NO_DEV $IGNORE_PLATFORM_REQS"
 echo "::group::$COMPOSER_COMMAND"
 $COMPOSER_COMMAND
 echo "::endgroup::"
-/composer/vendor/bin/phparkitect $*
+/composer/vendor/bin/phparkitect $* $TARGET_PHP_VERSION
