97db494799245c115bffd83b730c1f99797354eb
AlessandroMinoccheri <alessandro.minoccheri@ideato.info>
2022-01-09 23:32:12 +0100

    fix arg name

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,13 +12,13 @@ if [ "$REQUIRE_DEV" = "true" ]; then
     NO_DEV=""
 fi
 
-TARGET_PHP_VERSION=""
-if [ "TARGET_PHP_VERSION" != "" ]; then
-    TARGET_PHP_VERSION=" --target-php-version=$TARGET_PHP_VERSION"
+PHP_VERSION=""
+if [ "PHP_VERSION" != "" ]; then
+    PHP_VERSION=" --target-php-version=PHP_VERSION"
 fi
 
 COMPOSER_COMMAND="composer install --no-progress $NO_DEV $IGNORE_PLATFORM_REQS"
 echo "::group::$COMPOSER_COMMAND"
 $COMPOSER_COMMAND
 echo "::endgroup::"
-/composer/vendor/bin/phparkitect $* $TARGET_PHP_VERSION
+/composer/vendor/bin/phparkitect $* PHP_VERSION
